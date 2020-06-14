c78d90276fefa7b57795e5b59b3d71a77ccd77e0
Abel Segui <Abelsegui@hotmail.com>
2020-06-14 03:37:51 +0000

    first commit

--- src/js/views/single.js
+++ src/js/views/single.js
@@ -5,19 +5,7 @@ import { Context } from "../store/appContext";
 
 export const Single = props => {
 	const { store, actions } = useContext(Context);
-	return (
-		<div className="jumbotron">
-			<h1 className="display-4">This will show the demo element: {store.demo[props.match.params.theid].title}</h1>
-
-			<hr className="my-4" />
-
-			<Link to="/">
-				<span className="btn btn-primary btn-lg" href="#" role="button">
-					Back home
-				</span>
-			</Link>
-		</div>
-	);
+	return <div className="jumbotron">Single Page</div>;
 };
 
 Single.propTypes = {
